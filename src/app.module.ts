5649a73867988fd29cc75e071e63900f463ad8f1
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-07 21:58:00 +0700

    Durable Providers in-depth - Durable Providers 2: i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,8 +13,11 @@ import { DataSourceModule } from './data-source/data-source.module';
 import { UsersModule } from './users/users.module';
 import { ContextIdFactory } from '@nestjs/core';
 import { AggregateByTenantContextIdStrategy } from './core/aggregate-by-tenant.strategy';
+import { I18nModule } from './i18n/i18n.module';
+import { AggregateByLocaleContextIdStrategy } from './core/aggregate-by-locale.strategy';
 
-ContextIdFactory.apply(new AggregateByTenantContextIdStrategy());
+ContextIdFactory.apply(new AggregateByLocaleContextIdStrategy());
+// ContextIdFactory.apply(new AggregateByTenantContextIdStrategy());
 
 @Module({
   imports: [
@@ -29,6 +32,7 @@ ContextIdFactory.apply(new AggregateByTenantContextIdStrategy());
     PaymentsModule,
     DataSourceModule,
     UsersModule,
+    I18nModule,
     // ⚠️  Alternatively:
     // HttpClientModule.registerAsync({
     //   useFactory: () => ({ baseUrl: 'http://nestjs.com' }),

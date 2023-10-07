59ee421fb73c955ea0dc6e74f254ce132b6f76e0
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-07 13:24:42 +0700

    Misc Advanced Topics 🔎 - Explicit vs Implicit Dependencies

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CoffeesService } from './coffees.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+} from '@nestjs/common';
+import { CoffeesDataSource, CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { COFFEES_DATA_SOURCE } from './coffees.constants';
 
 @Controller('coffees')
 export class CoffeesController {
-  constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(
+    private readonly coffeesService: CoffeesService,
+    // try this two lines and see build/coffees.controller.js
+    // private readonly dataSource: CoffeesDataSource,
+    @Inject(COFFEES_DATA_SOURCE) private readonly dataSource: CoffeesDataSource,
+  ) {}
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {

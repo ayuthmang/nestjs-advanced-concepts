761a8ce20c2e76c8e947d7f3e1709ddcb1825d6b
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-07 16:44:13 +0700

    Misc Advanced Topics 🔎 - Building Configurable Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 // import { CronModule } from './cron/cron.module';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
+import { HttpClientModule } from './http-client/http-client.module';
 
 @Module({
-  imports: [CoffeesModule, SchedulerModule, FibonacciModule],
+  imports: [
+    CoffeesModule,
+    SchedulerModule,
+    FibonacciModule,
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }), // 👈
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }), // 👈
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }), // 👈
+    // ⚠️  Alternatively:
+    // HttpClientModule.registerAsync({
+    //   useFactory: () => ({ baseUrl: 'http://nestjs.com' }),
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/http-client/http-client.module.ts
@@ -0,0 +1,31 @@
+import { DynamicModule, Inject, Module } from '@nestjs/common';
+import {
+  ASYNC_OPTIONS_TYPE,
+  ConfigurableModuleClass,
+  HTTP_MODULE_OPTIONS,
+  OPTIONS_TYPE,
+} from './http-client.definition';
+
+@Module({})
+export class HttpClientModule extends ConfigurableModuleClass {
+  constructor(
+    @Inject(HTTP_MODULE_OPTIONS) private options: typeof OPTIONS_TYPE,
+  ) {
+    console.log(options);
+    super();
+  }
+
+  static register(options: typeof OPTIONS_TYPE): DynamicModule {
+    return {
+      // your custom logic here
+      ...super.register(options),
+    };
+  }
+
+  static registerAsync(options: typeof ASYNC_OPTIONS_TYPE): DynamicModule {
+    return {
+      // your custom logic here
+      ...super.registerAsync(options),
+    };
+  }
+}
